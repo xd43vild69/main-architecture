85682ecef85977c7fe94e2883f4ffd95ef0b7cd1
xd43vild69 <xd43vild69@gmail.com>
2019-04-09 12:09:02 -0500

    Init D13 architecture main project 2019

---
+++ Migrations/Migrations/Context/MigrationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using DTO;
+
+namespace Migrations.Context
+{
+    public class MigrationContext : DbContext
+    {
+        public MigrationContext() : base("StringConn")
+        {
+
+        }
+        public DbSet<Animal> Animals { get; set; }
+        //public DbSet<Schedule> Schedules { get; set; }
+        //public DbSet<TypeSchedule> TypeSchedules { get; set; }
+    }
+}
---
+++ Migrations/Migrations/MigrationMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migrations
+{
+    public class MigrationMain
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
